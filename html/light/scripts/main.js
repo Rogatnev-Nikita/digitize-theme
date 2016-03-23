63d0de7dfc61cb6a4c56c0f159ce97bc1bc58c37
Rogatnev Nikita <mail@rogatnev.ru>
2016-03-24 00:04:10 +0300

    Portfolio start

--- html/light/scripts/main.js
+++ html/light/scripts/main.js
@@ -19,23 +19,23 @@ var menu_btn = document.querySelector(".main-header__navigation-item");
 var menu_popup = document.querySelector(".main-header__menu");
 
 menu_btn.addEventListener("click", function(event) {
-    // when main menu is closed
-    if (menu_popup.classList.contains("main-header__menu--closed")) {
-        menu_popup.classList.remove("main-header__menu--closed");
-        menu_btn.classList.add("main-header__navigation-item--active");
-    }
     // when main menu is opened
-    else{
-        menu_popup.classList.add("main-header__menu--closed");
+    if (menu_popup.classList.contains("main-header__menu--opened")) {
+        menu_popup.classList.remove("main-header__menu--opened");
         menu_btn.classList.remove("main-header__navigation-item--active");
+    }
+    // when main menu is closed
+    else{
+        menu_popup.classList.add("main-header__menu--opened");
+        menu_btn.classList.add("main-header__navigation-item--active");
     }
 });
 
 
 $(document).keydown(function(e) {
     if( e.keyCode === 27 ) {
-        if (!menu_popup.classList.contains("main-header__menu--closed")) {
-            menu_popup.classList.add("main-header__menu--closed");   
+      if (menu_popup.classList.contains("main-header__menu--opened")) {
+            menu_popup.classList.remove("main-header__menu--opened");
             menu_btn.classList.remove("main-header__navigation-item--active");
         }
     }
