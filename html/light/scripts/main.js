7871f00cd3a4148152b88e4a48e6f8a8214ae566
Rogatnev Nikita <mail@rogatnev.ru>
2016-03-24 02:48:11 +0300

    Portfolio alfa

--- html/light/scripts/main.js
+++ html/light/scripts/main.js
@@ -1,42 +1,57 @@
-// slideScroll
 $(document).ready(function() {
+    // slideScroll
     if ($(window).width() > 960) {
         $('#fullpage').fullpage({
             'scrollBar': true,
             'navigation': true,
-            'afterLoad': function(anchorLink, index){}
+            'afterLoad': function(anchorLink, index) {}
         });
     }
-});
-
-$(document).on('click', '.scroll-to-next', function(){
-    $.fn.fullpage.moveSectionDown();
-});
+    $(document).on('click', '.scroll-to-next', function() {
+        $.fn.fullpage.moveSectionDown();
+    });
 
 
-// main menu
-var menu_btn = document.querySelector(".main-header__navigation-item");
-var menu_popup = document.querySelector(".main-header__menu");
 
-menu_btn.addEventListener("click", function(event) {
-    // when main menu is opened
-    if (menu_popup.classList.contains("main-header__menu--opened")) {
-        menu_popup.classList.remove("main-header__menu--opened");
-        menu_btn.classList.remove("main-header__navigation-item--active");
-    }
-    // when main menu is closed
-    else{
-        menu_popup.classList.add("main-header__menu--opened");
-        menu_btn.classList.add("main-header__navigation-item--active");
-    }
-});
+    // portfolio]
+    $('.portfolio__item-info').hide()
+    $('.portfolio__item').mouseenter(function() {
+        $(this).find('.portfolio__item-info').fadeIn(300);
+        $(this).css("cursor", "pointer");
+    });
+
+    $('.portfolio__item').mouseleave(function() {
+        $(this).find('.portfolio__item-info').fadeOut(300)
+    });
 
 
-$(document).keydown(function(e) {
-    if( e.keyCode === 27 ) {
-      if (menu_popup.classList.contains("main-header__menu--opened")) {
+
+    // main menu
+    var menu_btn = document.querySelector(".main-header__navigation-item");
+    var menu_popup = document.querySelector(".main-header__menu");
+
+    menu_btn.addEventListener("click", function(event) {
+        // when main menu is opened
+        if (menu_popup.classList.contains("main-header__menu--opened")) {
             menu_popup.classList.remove("main-header__menu--opened");
             menu_btn.classList.remove("main-header__navigation-item--active");
         }
-    }
+        // when main menu is closed
+        else {
+            menu_popup.classList.add("main-header__menu--opened");
+            menu_btn.classList.add("main-header__navigation-item--active");
+        }
+    });
+
+
+    $(document).keydown(function(e) {
+        if (e.keyCode === 27) {
+            if (menu_popup.classList.contains("main-header__menu--opened")) {
+                menu_popup.classList.remove("main-header__menu--opened");
+                menu_btn.classList.remove("main-header__navigation-item--active");
+            }
+        }
+    });
+
+
 });
