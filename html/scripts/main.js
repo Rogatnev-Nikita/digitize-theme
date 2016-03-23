2503574080663cff8c8e4e249c7f8effc26903b4
Rogatnev Nikita <mail@rogatnev.ru>
2016-03-23 18:44:26 +0300

    New menu

--- html/scripts/main.js
+++ html/scripts/main.js
@@ -15,24 +15,27 @@ $(function() {
 });
 
 
-// mobile menu
+// main menu
+var menu_open = document.querySelector(".main-header__navigation-item--open-btn");
+var menu_close = document.querySelector(".main-header__navigation-item--close-btn");
+
 var menu = document.querySelector(".main-header__navigation");
-var menu_open = document.querySelector(".main-header__navigation--open");
-var menu_close = document.querySelector(".main-header__navigation--close");
-var menu_popup = document.querySelector(".main-header__menu-item-list");
+var menu_popup = document.querySelector(".main-header__menu");
 
 menu_open.addEventListener("click", function(event) {
-    if (menu_popup.classList.contains("main-header__manu-item-list--closed")) {
-        menu_popup.classList.remove("main-header__manu-item-list--closed");
-        menu_open.classList.remove("main-header__navigation--active");
-        menu_close.classList.add("main-header__navigation--active");
+    if (menu_popup.classList.contains("main-header__menu--closed")) {
+        menu_popup.classList.remove("main-header__menu--closed");
+
+        menu_open.classList.remove("main-header__navigation-item--active");
+        menu_close.classList.add("main-header__navigation-item--active");
     }
 });
 
 menu_close.addEventListener("click", function(event) {
-    if (!menu_popup.classList.contains("main-header__manu-item-list--closed")) {
-        menu_popup.classList.add("main-header__manu-item-list--closed");
-        menu_close.classList.remove("main-header__navigation--active");
-        menu_open.classList.add("main-header__navigation--active");
+    if (!menu_popup.classList.contains("main-header__menu--closed")) {
+        menu_popup.classList.add("main-header__menu--closed");
+
+        menu_close.classList.remove("main-header__navigation-item--active");
+        menu_open.classList.add("main-header__navigation-item--active");
     }
 });
